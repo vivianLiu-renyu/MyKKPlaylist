b04ab71d26d5f44c1a670725fef63046e921609e
Vivian Liu <vivianliu@kkbox.com>
2019-08-12 14:59:01 +0800

    modify get_access_token.sh

--- MyKKPlaylist/build-phase/get_access_token.sh
+++ MyKKPlaylist/build-phase/get_access_token.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-curl -d "client_id=${ID}&client_secret=${SECRET}&grant_type=client_credentials" "https://account.kkbox.com/oauth2/token" > MyKKPlaylist/accessToken.json
+curl -d "client_id=f83d449bf6233c25b73330413dcb313b&client_secret=bbe1d1310eb22e2d6c4517c4a5907e09&grant_type=client_credentials" "https://account.kkbox.com/oauth2/token" > MyKKPlaylist/accessToken.json
